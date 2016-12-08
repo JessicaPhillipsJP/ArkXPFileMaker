c40652c52b0c424879e8c395030295e2ec1638a5
Jessica <jessicaphillips.jp@gmail.com>
2016-12-08 11:41:15 -0600

    Add files via upload

---
+++ writeengram/src/writeengram/WriteUI.java
@@ -0,0 +1,28 @@
+package writeengram;
+
+import javax.swing.JFrame;
+
+public class WriteUI {
+	
+	public WriteUI(){
+		
+	}
+	
+	public void createUI(){
+		JFrame frame = new JFrame("Make Server Settings for Ark");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//label asking player or player and dinos
+		//combo box for player or player and dinos
+		
+		//label asking for engram override
+		//checkbox for engrams 
+		
+		//label for level to go to
+		//text field asking for level to go to
+		
+		//ok button
+		//cancel button
+		
+	}
+
+}
