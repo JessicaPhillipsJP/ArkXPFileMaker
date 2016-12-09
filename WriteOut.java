fca08f1fd86f88f614f8533b4b7c5a52d5437981
Jessica <jessicaphillips.jp@gmail.com>
2016-12-09 13:25:57 -0600

    Add files via upload

--- WriteOut.java
+++ WriteOut.java
@@ -1,34 +1,64 @@
-package writeengram;
+package uidesign;
+
+import java.awt.Component;
+
+import javax.swing.JOptionPane;
 
 public class WriteOut {
-	
-	
-	public void xpOverride(){
+
+	private String comboBox;
+	private boolean checkBox;
+	private String textField;
+	private int getLevelCap = 0;
+	Component frame;
+
+	private boolean playerDino = false;
+
+	public WriteOut(String comboBox, boolean checkBox, String textField, Component frame) {
+		this.comboBox=comboBox;
+		this.checkBox=checkBox;
+		this.textField=textField;
+		this.frame=frame;
+	}
+
+	public void printIt() {
+		getLevelCap = Integer.parseInt(textField);
+		if(checkComboBox() == true){
+			printXP();
+		}
+		else{
+			printXP();
+			printXP();
+		}
+		if(checkBox == true)
+			for(int i = 0; i<getLevelCap+1; i++){
+				System.out.println("OverridePlayerLevelEngramPoints="+ i*10);
+			}
+	}
+
+
+	public boolean checkComboBox(){
+		if(comboBox.toString().trim().equals("Players Only")){
+			playerDino = true;
+		}
+		return playerDino;
+	}
+
+	public void printXP(){
 		System.out.println("[/script/shootergame.shootergamemode]");
 		System.out.print("LevelExperienceRampOverrides=(");
-		for(int i = 0; i<501; i++){
+		for(int i = 0; i<getLevelCap+1; i++){
 			if(i == 0){
 				System.out.print("ExperiencePointsForLevel["+i+"]="+2+",");
 			}
-			else if( i <500){
-			System.out.print("ExperiencePointsForLevel["+i+"]="+i*5+",");
+			else if( i <getLevelCap){
+				System.out.print("ExperiencePointsForLevel["+i+"]="+i*5+",");
 			}
 			else{
 				System.out.print("ExperiencePointsForLevel["+i+"]="+i*5+")");
 			}
-			
+
 		}
 	}
-	public void engramOverride(){
-		for(int i = 0; i<501; i++){
-			System.out.println("OverridePlayerLevelEngramPoints="+ i*10);
-		}
-		
-	}
-//	public static void main(String[] args){
-//		WriteOut out = new WriteOut();
-//		out.xpOverride();
-//		out.engramOverride();
-//	}
 
 }
